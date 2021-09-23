0b3fdf8b91fd63895430707d36ccf05f89a63849
Jim <jimzhu0602@gmail.com>
2021-09-24 03:22:45 +1200

    Prototype route finding

---
+++ routes_j.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy.core.numeric import NaN
+from matplotlib import pyplot as plt
+from numpy.lib.function_base import interp
+from scipy.interpolate import interp1d
+import itertools
+
+def main():
+
+	pass
+
+
+if __name__ == "__main__":
+    main()
