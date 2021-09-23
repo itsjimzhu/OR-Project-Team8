d47cc3106b0af0a6a1f9f078dd0da9f0c01bc039
Jim <jimzhu0602@gmail.com>
2021-09-24 04:09:17 +1200

    Generates routes matrix

--- routes_j.py
+++ routes_j.py
@@ -4,11 +4,61 @@ from matplotlib import pyplot as plt
 from numpy.lib.function_base import interp
 from scipy.interpolate import interp1d
 import itertools
+from math import comb
+from itertools import combinations
+import pandas as pd
 
 def main():
 
-	pass
+    # Creating a matrix of possible length 3 routes in a region of N stores.
+	# The matrix should have N rows and nC3 columns
+    pass
+
+def route(df):
+    """ generates matrix of potential length three routes
+	
+		Parameters
+		----------
+		df :    Pandas dataframe
+            Should contain read_csv data from Locations file of ten stores (changeable)
+			
+		Returns
+        stores :    Pandas dataframe
+            First col cis index of store names, remaining columns are possible combinations of stores
+		-------
+		None
+	"""
+    
+
+    # Creating a pandas dataframe of possible length 3 routes in a region of N stores.
+	# The dataframe should have N rows and nC3 (+1) columns
+    # Column of store names and idx
+    stores = df[['Store']]
+
+    # Columns for combos of three stores.
+    n = 11
+    k = 3
+    routes = np.array(
+        [
+            [1 if i in comb else 0 for i in range(n)]
+            for comb in combinations(np.arange(n), k)
+        ]
+    )
+ 
+    # Loop through each route and add as a column to the stores df
+    for i in range(len(routes)):
+        copy = routes[i,:].T
+        stores.insert(1,i,copy)
+
+    return stores
 
 
 if __name__ == "__main__":
-    main()
+    N = 10
+    df = pd.read_csv("WoolworthsLocations.csv")
+    df1 = df.loc[0:10,:]
+    routes = route(df1)
+    pass
+    # TODO
+    # Function to calculate time cost for each of the routes generated.
+    
