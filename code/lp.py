0db936328bd14f9d70b8e32a42b3f272cb282281
Shalin Naidoo <shalin.naidoo.personal@gmail.com>
2021-09-24 14:29:05 +1200

    Generic LP code

--- code/lp.py
+++ code/lp.py
@@ -1,55 +1,60 @@
 # imports
 from pulp import *
 
-# list of A list of all the roll lengths is created
-LenOpts = ["5", "7", "9"]  # list of stores in our case
 
-# A dictionary of the demand for each roll length is created
-rollDemand = {"5": 150,
-              "7": 200,
-              "9": 300}  # store demand
+def Kevin():
+    # list of A list of all the roll lengths is created
+    LenOpts = ["5", "7", "9"]  # list of stores in our case
 
-# A list of all the patterns is created
-PatternNames = ["A", "B", "C"]  # routes
+    # A dictionary of the demand for each roll length is created
+    rollDemand = {"5": 150,
+                  "7": 200,
+                  "9": 300}  # store demand
 
-# Creates a list of the number of rolls in each pattern for each different roll length
-patterns = [  # A B C
-    [0, 2, 2],  # 5
-    [1, 1, 0],  # 7
-    [1, 0, 1]  # 9
-]  # route x node in our case (all 1's)
+    # A list of all the patterns is created
+    PatternNames = ["A", "B", "C"]  # routes
 
-# The cost of each 20cm long sponge roll used
-cost = 1  # time per route
+    # Creates a list of the number of rolls in each pattern for each different roll length
+    patterns = [  # A B C
+        [0, 2, 2],  # 5
+        [1, 1, 0],  # 7
+        [1, 0, 1]  # 9
+    ]  # route x node in our case (all 1's)
 
-# The pattern data is made into a dictionary
-patterns = makeDict([LenOpts, PatternNames], patterns, 0)  # can now reference using strings instead of indexes
+    # The cost of each 20cm long sponge roll used
+    cost = 1  # time per route
 
-# The problem variables of the number of each pattern to make are created
-vars = LpVariable.dicts("Patt", PatternNames, 0, None, Lpłnteger)
+    # The pattern data is made into a dictionary
+    patterns = makeDict([LenOpts, PatternNames], patterns, 0)  # can now reference using strings instead of indexes
 
-# The variable 'prob' is created
-prob = LpProblem("Cutting Stock Problem", LpMinimize)
+    # The problem variables of the number of each pattern to make are created
+    vars = LpVariable.dicts("Patt", PatternNames, 0, None, 'Integer')
 
-# The objective function is entered: the total number of large rolls used + the fixed cost of each
-prob += lpSum([vars[i] * cost for i in PatternNames]), "Production Cost"
+    # The variable 'prob' is created
+    prob = LpProblem("CuttingStockProblem", LpMinimize)
 
-# The demand minimum constraint is entered
-for i in LenOpts:
-    prob += lpSum([vars[j] * patterns[i][j] for j in PatternNames]) >= rollDemand[i], "Ensuring enough % cm"
+    # The objective function is entered: the total number of large rolls used + the fixed cost of each
+    prob += lpSum([vars[i] * cost for i in PatternNames]), "ProductionCost"
 
-# The problem data is written to an .lp file
-prob.writeLP("SpongeRollProblem. lp")
+    # The demand minimum constraint is entered
+    for i in LenOpts:
+        prob += lpSum([vars[j] * patterns[i][j] for j in PatternNames]) >= rollDemand[i]
 
-# The problem is solved using PuLP's choice of Solver
-prob.solve()
+    # The problem data is written to an .lp file
+    prob.writeLP("SpongeRollProblem. lp")
 
-# The status of the solution is printed to the screen
-print("Status:", LpStatus[prob.status])
+    # The problem is solved using PuLP's choice of Solver
+    prob.solve()
 
-# Each of the variables is printed with it's resolved optimum value
-for v in prob.variables():
-    print(v.name, "-", v.varValue)
+    # The status of the solution is printed to the screen
+    print("Status:", LpStatus[prob.status])
 
-# The optimised objective function value is printed to the screen
-print("Production Costs = ", value(prob.objective))
+    # Each of the variables is printed with it's resolved optimum value
+    for v in prob.variables():
+        print(v.name, "-", v.varValue)
+
+    # The optimised objective function value is printed to the screen
+    print("Production Costs = ", value(prob.objective))
+
+if __name__ == "__main__":
+    Kevin()
