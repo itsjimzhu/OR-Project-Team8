f19ade6ed943d4009a1bdda2f67dc557dfabb334
dlee339 <80794055+dlee339@users.noreply.github.com>
2021-09-29 20:20:49 +1300

    Added Reading CSV into Pandas code

--- code/Bits_and_bobs.py
+++ code/Bits_and_bobs.py
@@ -3,3 +3,9 @@ import pandas as pd
 df = pd.DataFrame({"Route 1": [x, y], "Route 2": [i, j]})
 df_to_array = df.to_numpy()
 array([[x, y], [i, j]])
+
+# Importing CSV files into Pandas
+import pandas as pd
+df = pd.read_csv('Name_of_data_file.csv')
+print(df.to_string()) 
+
