a8b5e73be5f378078347c9f2137b9308bc838a05
Joshua Sampson <jsam271@aucklanduni.ac.nz>
2021-10-05 15:14:32 +1300

    RouteVis formating improved

--- code/routeSizeVis.py
+++ code/routeSizeVis.py
@@ -6,23 +6,28 @@ from pulp import *
 import os
 import time
 
+'''This code simulates random routes of given length and calculates
+    what percentage have valid demand '''
+
 if __name__ == "__main__":
     initial = pd.read_csv("code" + os.sep + "data" + os.sep + "DemandLongPivot.csv", index_col=0)
     initial.set_index('Store')
-    weekday = initial[initial['day'].isin(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])]
+    weekday = initial[initial['day'].isin(['Saturday'])]
+    weekday = weekday[weekday['demand']>0]
     pivoted = weekday.pivot(index = 'Store', columns = 'date', values = 'demand')
     count = 0
     number = 0
-    testing_number = 6
+    daily_sims = 10000
+    testing_number = 5
     for j in range(len(pivoted.columns)):
-        randi = np.random.randint(0,pivoted.shape[0],10000 + testing_number)
-        for i in range(10000):
+        randi = np.random.randint(0,pivoted.shape[0],daily_sims + testing_number)
+        for i in range(daily_sims):
             sums = 0
             for len in range(testing_number):
                 sums += pivoted.iloc[randi[i+len],j]
             count += 1
             if sums < 28:
                 number += 1
-    print(number/count)
-    print(number)
-    print(count)
+    print('Viable Routes: ' + str(number))
+    print('Total Routes Simulated: ' + str(count))
+    print('Percentage Valid: ' + str(number/count))
